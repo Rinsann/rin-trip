c1cddb54f37129864163e84179b19ef40828b23e
rin <ftaocoder@gmail.com>
2023-02-19 00:58:49 +0800

    loading加载 请求响应拦截器

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="app">
     <router-view />
     <tab-bar v-if="!route.meta.hideTabBar" />
+    <Loading />
     <!-- <tab-bar /> -->
   </div>
 </template>
@@ -9,8 +10,9 @@
 <script setup>
 import TabBar from "@/components/tab-bar/tab-bar.vue";
 import { useRoute } from "vue-router";
-
+import Loading from "./components/loading/loading.vue";
 const route = useRoute();
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
