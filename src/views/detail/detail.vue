2b359ac819fd726fb7d4f3373c6affbe0562ba08
rin <ftaocoder@gmail.com>
2023-02-19 01:23:32 +0800

    修改主题色,搭建导航

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,14 +1,25 @@
-import { useRoute } from 'vue-router';
 <template>
   <div class="detail top-page">
-    <h2>detail: {{ paramsId }}</h2>
+    <van-nav-bar
+      title="房屋详情"
+      left-text="旅途"
+      left-arrow
+      @click-left="onClickLeft"
+    />
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-const route = useRoute()
-const paramsId = route.params.id
+import { useRoute, useRouter } from 'vue-router';
+
+const router =  useRouter()
+const route = useRoute();
+const paramsId = route.params.id;
+
+// 监听返回按钮的点击
+const onClickLeft = ()=>{
+  router.push('/')
+}
 </script>
 
 <style lang="less" scoped></style>
