aac6e39b0278c2d0779e90db3b3970234c7b43c8
rin <ftaocoder@gmail.com>
2023-02-19 13:49:12 +0800

    集成百度地图

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -13,6 +13,12 @@
       <detail-facility
         :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"
       />
+      <detail-landlord :landlord="mainPart.dynamicModule.landlordModule" />
+      <detail-comment :comment="mainPart.dynamicModule.commentModule" />
+      <detail-notice
+        :order-rules="mainPart.dynamicModule.rulesModule.orderRules"
+      />
+      <detail-map :position="mainPart.dynamicModule.positionModule" />
     </div>
   </div>
 </template>
@@ -24,6 +30,10 @@ import { getDetailInfos } from "@/services";
 import DetailSwipe from "./cpns/detail_01-swipe.vue";
 import DetailInfos from "./cpns/detail_02-infos.vue";
 import DetailFacility from "./cpns/detail_03-facility.vue";
+import DetailLandlord from "./cpns/detail_04-landlord.vue";
+import DetailComment from "./cpns/detail_05-comment.vue";
+import DetailNotice from "./cpns/detail_06-notice.vue";
+import DetailMap from "./cpns/detail_07-map.vue";
 
 const router = useRouter();
 const route = useRoute();
