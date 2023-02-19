f3f445c51df2498a15b16762f3d81a0f76556811
rin <ftaocoder@gmail.com>
2023-02-19 10:27:18 +0800

    引入轮播图

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -6,20 +6,32 @@
       left-arrow
       @click-left="onClickLeft"
     />
+
+    <div class="main" v-if="mainPart">
+      <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
-
-const router =  useRouter()
+import { ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { getDetailInfos } from "@/services";
+import DetailSwipe from "./cpns/detail-swipe.vue";
+const router = useRouter();
 const route = useRoute();
-const paramsId = route.params.id;
+
+// 发送网络请求获取数据
+const detailInfos = ref({});
+const mainPart = computed(() => detailInfos.value.mainPart);
+getDetailInfos(route.params.id).then((res) => {
+  detailInfos.value = res.data;
+});
 
 // 监听返回按钮的点击
-const onClickLeft = ()=>{
-  router.push('/')
-}
+const onClickLeft = () => {
+  router.back();
+};
 </script>
 
 <style lang="less" scoped></style>
